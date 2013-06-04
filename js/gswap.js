cd6ec51757c994dd94bed04f4e62676890b079e5
Michael Gruber <michaelegruber@gmail.com>
2013-06-04 08:05:45 -0400

    added modifiable timing

--- js/gswap.js
+++ js/gswap.js
@@ -1,11 +1,15 @@
 (function($){ 
 
-    var GSwap = function() {
+    var GSwap = function(options) {
 
         var substitute = JSON.parse($('#gswapable').html());
         var target     = $('#gswap');
         var current    = 0;
         var howMany    = substitute.items.length;
+        var inTime     = options.inTime * 1000;
+        var outTime    = options.outTime * 1000;
+        var pauseTime  = options.pauseTime * 1000;
+        var totalTime  = inTime + outTime + pauseTime;
 
         this.swap = function() {
 
@@ -13,22 +17,32 @@
             current = (current < howMany - 1) ? current + 1 : 0;
 
             // Animate and swap
-            target.animate({"top":"0px", "opacity":1}, 500);
-            target.animate({"opacity": 0, "top":"10px"}, 500, function(){
+            target.animate({"top":"0px", "opacity":1}, inTime)
+                  .delay(pauseTime);
+            target.animate({"opacity": 0, "top":"10px"}, outTime, function(){
                 target.text(substitute.items[current]);
                 target.css({"top":"-10px", "opacity":0});
             });
 
             // Recall the swap function after animations are complete
-            setTimeout(arguments.callee, 1000);
+            setTimeout(arguments.callee, totalTime);
         }
 
         // ON INSTANTIATION
         target.css({ position: "relative" });
     };
 
-    $.fn.gswap = function() { 
-        var gswap = new GSwap();
+    $.fn.gswap = function(options) { 
+
+        var defaults = {
+            inTime: 0.5,  // (in seconds)
+            outTime: 0.5,
+            pauseTime: 0  // How long to pause between fade in and fade out
+        }
+        
+        var options = $.extend(defaults, options);
+
+        var gswap = new GSwap(options);
         
         gswap.swap();
     };
