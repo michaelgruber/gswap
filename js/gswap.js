07d15ed4160e655a7aa6554d646552dc9ddde52f
Michael Gruber <michaelegruber@gmail.com>
2013-06-03 20:13:30 -0400

    looping and fading

--- js/gswap.js
+++ js/gswap.js
@@ -1,12 +1,31 @@
 (function($){ 
-    $.fn.gswap = function() { 
+
+    var GSwap = function() {
+
         var substitute = JSON.parse($('#gswapable').html());
         var target     = $('#gswap');
+        var current    = 0;
+        var howMany    = substitute.items.length;
 
-        $.each(substitute.items, function(index, value) {
-            setTimeout(function() {
-                target.text(value);            
-            }, 1000 * index);
-        });
-    } 
+        this.swap = function() {
+
+            // Increment current or set loop to start
+            current = (current < howMany - 1) ? current + 1 : 0;
+
+            // Animate and swap
+            target.animate({opacity: 0}, 500, function(){
+                target.text(substitute.items[current]);
+            });
+            target.animate({opacity: 1}, 500);
+
+            // Recall the swap function after animations are complete
+            setTimeout(arguments.callee,1000);
+        }
+    };
+
+    $.fn.gswap = function() { 
+        var gswap = new GSwap();
+
+        gswap.swap();
+    };
 })(jQuery);
