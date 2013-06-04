be1cb0f6daca25d96b5e1924378cff1108d29f16
Michael Gruber <michaelegruber@gmail.com>
2013-06-03 23:33:08 -0400

    added sliding animation

--- js/gswap.js
+++ js/gswap.js
@@ -13,19 +13,23 @@
             current = (current < howMany - 1) ? current + 1 : 0;
 
             // Animate and swap
-            target.animate({opacity: 0}, 500, function(){
+            target.animate({"top":"0px", "opacity":1}, 500);
+            target.animate({"opacity": 0, "top":"10px"}, 500, function(){
                 target.text(substitute.items[current]);
+                target.css({"top":"-10px", "opacity":0});
             });
-            target.animate({opacity: 1}, 500);
 
             // Recall the swap function after animations are complete
-            setTimeout(arguments.callee,1000);
+            setTimeout(arguments.callee, 1000);
         }
+
+        // ON INSTANTIATION
+        target.css({ position: "relative" });
     };
 
     $.fn.gswap = function() { 
         var gswap = new GSwap();
-
+        
         gswap.swap();
     };
 })(jQuery);
